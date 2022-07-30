cb2b1a573077aff5a2e2aa8e12ed7cf8bb3e0dce
C Ram <cmstrickland@gmail.com>
2022-07-30 00:06:36 -0500

    overlay complete

---
+++ scripts.js
@@ -0,0 +1,47 @@
+
+        // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+        /*link.onclick = function () {
+            navBarCollapse()
+        };
+
+        function navBarCollapse() {
+            if (document.nav.height > 80) {
+                document.getElementById("navbar").style.padding = "30px 10px";
+                document.getElementById("nav").style.fontSize = "25px";
+            } else {
+                return;
+            }
+        }*/
+        /*
+        function openNav() {
+            document.getElementById("collapsibleNavbar").style.width = "100%";
+          }
+          
+          function closeNav() {
+            //document.getElementById("collapsibleNavbar").collapse();
+            console.log("You made it to closeNav()");
+            document.getElementById("collapsibleNavbar").style.height = "50px";
+          }
+          */
+        //   /* Open */
+        //   function openNav() {
+        //     console.log("openNav()");
+        //     document.getElementById("myNav").style.height = "100%";
+        //   }
+          
+        //   /* Close */
+        //   function closeNav() {
+        //     console.log("closeNav()");
+        //     document.getElementById("myNav").style.height = "0%";
+        //   }
+
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+  } 
+          
