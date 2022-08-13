d65abd1e1f10ebae59181ec43eba440f525a4111
C Ram <cmstrickland@gmail.com>
2022-08-13 00:44:21 -0500

    added independently functioning carousels, see scripts.js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,3 @@
-
-        // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-        /*link.onclick = function () {
-            navBarCollapse()
-        };
-
-        function navBarCollapse() {
-            if (document.nav.height > 80) {
-                document.getElementById("navbar").style.padding = "30px 10px";
-                document.getElementById("nav").style.fontSize = "25px";
-            } else {
-                return;
-            }
-        }*/
-        /*
-        function openNav() {
-            document.getElementById("collapsibleNavbar").style.width = "100%";
-          }
-          
-          function closeNav() {
-            //document.getElementById("collapsibleNavbar").collapse();
-            console.log("You made it to closeNav()");
-            document.getElementById("collapsibleNavbar").style.height = "50px";
-          }
-          */
-        //   /* Open */
-        //   function openNav() {
-        //     console.log("openNav()");
-        //     document.getElementById("myNav").style.height = "100%";
-        //   }
-          
-        //   /* Close */
-        //   function closeNav() {
-        //     console.log("closeNav()");
-        //     document.getElementById("myNav").style.height = "0%";
-        //   }
-
 /* Open */
 function openNav() {
     document.getElementById("myNav").style.height = "100%";
@@ -48,20 +11,62 @@ function openNav() {
   /* Carousel 
   https://www.w3schools.com/w3css/w3css_slideshow.asp
   */
+//  declare all project slideshows
+  let proj = new Array('mySlides', 'rolSlides');
   var slideIndex = 1;
-  showDivs(slideIndex);
+  //this var is the proj class sent by clicking arrows on projects.html
+  var fromArrow;
+  showDivs(slideIndex, proj);
   
-  function plusDivs(n) {
-    showDivs(slideIndex += n);
+  //called from projects.jtml arrows fromArrow can be mySlides, rolSlides, etc
+  function plusDivs(n, fromArrow) {
+   console.log('you selected from the class: ', fromArrow);
+   console.log("you reached plusDivs");
+    showDivs(slideIndex += n, fromArrow);
+    
   }
   
-  function showDivs(n) {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
+  function showDivs(n, fromArrow) {
+    var i, j;
+    console.log('you reached showDivs with class: ', fromArrow);
+    // for (j = 0; j < proj.length; j++){
+      console.log('showDivs for loop now = ', proj[j])
+       //x is array of images
+    var x = document.getElementsByClassName(fromArrow);
+    
+    if (n > x.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = x.length
+    }
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
-    x[slideIndex-1].style.display = "block";  
+    x[slideIndex-1].style.display = "block";
+  //}  
   }
+
+  //load carousels on page load
+  function onloadDivs(n) {
+    var j;
+
+    for (j = 0; j < proj.length; j++){
+    var x = document.getElementsByClassName(proj[j]);
+    
+    if (n > x.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = x.length
+    }
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";
+
+  }
+  }
+  
+
+  
